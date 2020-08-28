7bc1b2430115a88e539ce8f742192e88b21c5752
Combatd <combatdstar@gmail.com>
2020-08-28 05:32:17 -0700

    Render TodoList in App.js

--- step1-05/todolist/src/App.js
+++ step1-05/todolist/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import { TodoHeader } from './components/TodoHeader';
 import { TodoFooter } from './components/TodoFooter';
+import { TodoList } from './components/TodoList';
 
 export default class TodoApp extends React.Component {
   render() {
     return (
       <div>
         <TodoHeader />
+        <TodoList />
         <TodoFooter />
       </div>
     );
