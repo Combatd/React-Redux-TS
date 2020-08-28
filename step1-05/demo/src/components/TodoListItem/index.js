30d754bceb7009822a43c855b01cebf79878ac9d
Combatd <combatdstar@gmail.com>
2020-08-28 04:12:06 -0700

    Create TodoListItem functional component

---
+++ step1-05/demo/src/components/TodoListItem/index.js
@@ -0,0 +1,9 @@
+export const TodoListItem = (props) => {
+    return (
+        <li className="todo">
+            <label>
+                <input type="checkbox" /> Todo 1
+            </label>
+        </li>
+    )
+}
