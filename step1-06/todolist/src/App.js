63fd10cee58bc13f29bdfdec9243ce2aae1c6744
Combatd <combatdstar@gmail.com>
2020-09-02 02:15:43 -0700

    Define TodoApp constructor

--- step1-06/todolist/src/App.js
+++ step1-06/todolist/src/App.js
@@ -6,6 +6,33 @@ import { TodoFooter } from './components/TodoFooter';
 import { TodoList } from './components/TodoList';
 
 export default class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      todos: {
+        '04' : {
+          label: 'Todo 4',
+          completed: true
+        },
+        '03': {
+          label: 'Todo 3',
+          completed: false
+        },
+        '02': {
+          label: 'Todo 2',
+          completed: false
+        },
+        '01': {
+          label: 'Todo 1',
+          completed: false
+        }
+      },
+      filter: 'active'
+
+    }
+  }
+
   render() {
     return (
       <div>
