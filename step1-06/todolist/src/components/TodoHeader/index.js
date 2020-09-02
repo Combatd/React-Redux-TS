04e908e8d8acc4a522652d12ce11727ff5a32885
Combatd <combatdstar@gmail.com>
2020-09-02 12:13:33 -0700

    Pass down props and setState TodoHeader component

--- step1-06/todolist/src/components/TodoHeader/index.js
+++ step1-06/todolist/src/components/TodoHeader/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 export class TodoHeader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            labelInput: ''
+        }
+    }
+
+    _onChange(evt) {
+        this.setState({ labelInput : evt.target.value })
+    }
+
     render() {
+        const filter = this.props.filter;
         return (
             <header>
                 <h1>todos <small>(1.5 exercise)</small></h1>
                 <div className="addTodo">
-                    <input className="textfield" placeholder="add todo" />
+                    <input value={this.state.labelInput} onChange={(e) => this._onChange(e)} className="textfield" placeholder="add todo" />
                     <button className="submit">Add</button>
                 </div>
                 <nav className="filter">
