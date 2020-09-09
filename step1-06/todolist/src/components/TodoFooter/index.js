564ebe0c3be09cc2defddddca5d919df6b5a6f08
Combatd <combatdstar@gmail.com>
2020-09-08 17:23:57 -0700

    UFilter from this.props.todos in TodoFooter to get count of items left

--- step1-06/todolist/src/components/TodoFooter/index.js
+++ step1-06/todolist/src/components/TodoFooter/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 export class TodoFooter extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    componentDidMount() {
+
+    }
+
     render() {
+        const itemCount = Object.keys(this.props.todos).filter( (id) => { return !this.props.todos[id].completed } ).length 
         return(
             <footer>
-                <span>4 items left</span>
+                <span>{itemCount} items left</span>
                 <button className="submit">Clear Completed</button>
             </footer>
         )
